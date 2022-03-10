b2b8941a7c4352e65300a25667d65b89250015a0
Arian Baishya <arian.baishya2004@gmail.com>
2022-03-09 22:27:56 -0600

    Initial commit.

---
+++ app/src/main/java/me/arianb/usb_hid_client/MainActivity.java
@@ -0,0 +1,14 @@
+package me.arianb.usb_hid_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
