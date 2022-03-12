d494030a6b1dc1c93b80931fa0dfc9083b8cd817
arian <arian.baishya2004@gmail.com>
2022-03-11 21:30:49 -0600

    Make characters typed by pressing a key while holding shift work.

--- app/src/main/java/me/arianb/usb_hid_client/MainActivity.java
+++ app/src/main/java/me/arianb/usb_hid_client/MainActivity.java
@@ -1,7 +1,5 @@
 package me.arianb.usb_hid_client;
 
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.text.Editable;
@@ -10,6 +8,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.EditText;
+import static java.util.Map.entry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,16 +19,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 
 // TODO: make it detect all keys (function keys, SysRq, etc.)
 //       - i think i might just have to create some manual special buttons for that purpose
-
+// Backspaces stop working on empty edittext
+// any key after a "+" sends twice. i have no idea why.
 public class MainActivity extends AppCompatActivity {
     private EditText input;
     private Button btn;
 
     private String appFileDirectory;
     private String hidGadgetPath;
+
+    private Map<Character, String> translateChars;
+    private Map<Character, String> shiftChars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,35 @@ public class MainActivity extends AppCompatActivity {
 
         input = findViewById(R.id.etKeyboardInput);
         btn = findViewById(R.id.btnKeyboard);
+
+        translateChars = new HashMap<Character, String>();
+        shiftChars = new HashMap<Character, String>();
+
+        // Chars to translate to into string representing a key
+        translateChars.put(' ', "space");
+
+        // Chars that are represented by another key + shift
+        shiftChars.put('<', ",");
+        shiftChars.put('>', ".");
+        shiftChars.put('?', "/");
+        shiftChars.put(':', ";");
+        shiftChars.put('"', "'");
+        shiftChars.put('{', "[");
+        shiftChars.put('}', "]");
+        shiftChars.put('|', "\\");
+        shiftChars.put('~', "`");
+        shiftChars.put('!', "1");
+        shiftChars.put('@', "2");
+        shiftChars.put('#', "3");
+        shiftChars.put('$', "4");
+        shiftChars.put('%', "5");
+        shiftChars.put('^', "6");
+        shiftChars.put('&', "7");
+        shiftChars.put('*', "8");
+        shiftChars.put('(', "9");
+        shiftChars.put(')', "0");
+        shiftChars.put('_', "-");
+        shiftChars.put('+', "=");
 
         appFileDirectory = "/data/data/me.arianb.usb_hid_client";
         hidGadgetPath = appFileDirectory + "/hid-gadget";
@@ -62,47 +97,53 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(start + " " + before + " " + count);
                 if (before > count) { // I think this conditional is accurate but haven't finished testing
                     System.out.println("Backspace");
-                    sendKey("backspace");
+                    sendKey("backspace", false);
                 } else {
-                    CharSequence newChar = s.subSequence(s.length() - 1, s.length());
-                    System.out.println(newChar);
-                    sendKey(newChar.toString());
+                    char newChar = s.subSequence(s.length() - 1, s.length()).charAt(0);
+                    System.out.println("sendchar: " + newChar);
+                    String str = null;
+                    if((str = translateChars.get(newChar)) != null) {
+                        sendKey(str, false);
+                        System.out.println("if: " + str);
+                    } else if((str = shiftChars.get(newChar)) != null) {
+                        sendKey(str, true);
+                        System.out.println("elif: " + str);
+                    } else {
+                        sendKey(Character.toString(newChar), false);
+                        System.out.println("nomatch: " + shiftChars.get(newChar));
+                        System.out.println("else: " + newChar);
+                    }
                 }
             }
         });
     }
 
 
-    private void sendKey(String str) {
+    private void sendKey(String str, Boolean pressShift) {
         String options = "";
         String key = str;
-        if (str.length() == 1 && Character.isUpperCase(str.charAt(0))) {
+        if(pressShift) {
+            options = "--left-shift";
+        }
+
+        // Escape quotes
+        if(str.equals("\'")) {
+            key = "\\\'";
+        } else if(str.equals("\"")) {
+            key = "\\\"";
+        }
+
+        if(str.length() == 1 && Character.isUpperCase(str.charAt(0))) {
             options = "--left-shift";
             key = str.toLowerCase();
-            System.out.println(str);
+            //System.out.println(str);
         }
-        System.out.println(hidGadgetPath);
-        String[] shell = {"su", "-c", "echo " + key + " " + options + " | " + hidGadgetPath + " /dev/hidg0 keyboard"};
+        //System.out.println(hidGadgetPath);
+        String[] shell = {"su", "-c", "echo \'" + key + "\' " + options + " | " + hidGadgetPath + " /dev/hidg0 keyboard"};
         try {
             Process process = Runtime.getRuntime().exec(shell);
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(process.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(process.getErrorStream()));
-
-            // Read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            String s = null;
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-            // Read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
-
+            //System.out.println(printProcessStdOutput(process));
+            System.out.println(printProcessStdError(process));
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("ioexception");
@@ -145,5 +186,31 @@ public class MainActivity extends AppCompatActivity {
             Log.e("tag", "Failed to copy asset file: " + filename, e);
         }
         Log.d("tag", "Copy success: " + filename);
+    }
+
+    private String printProcessStdOutput(Process process) throws IOException {
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(process.getInputStream()));
+        // Read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        String returnStr = "";
+        while ((s = stdInput.readLine()) != null) {
+            returnStr += s + "\n";
+        }
+        return returnStr;
+    }
+
+    private String printProcessStdError(Process process) throws IOException {
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(process.getErrorStream()));
+        // Read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        String s = null;
+        String returnStr = "";
+        while ((s = stdError.readLine()) != null) {
+            returnStr += s + "\n";
+        }
+        return returnStr;
     }
 }
