23351cb2ac70f25dfb5a8cc2524ec63540da2d03
Arian Baishya <arian.baishya2004@gmail.com>
2022-03-16 17:20:08 -0500

    Reformat code.

--- app/src/main/java/me/arianb/usb_hid_client/MainActivity.java
+++ app/src/main/java/me/arianb/usb_hid_client/MainActivity.java
@@ -63,69 +63,69 @@ public class MainActivity extends AppCompatActivity {
 		shiftChars = new HashMap<>();
 
 		// Translate modifier keycodes into key
-        modifierKeys.put(113, "--left-ctrl");
-        modifierKeys.put(114, "--right-ctrl");
-        modifierKeys.put(59, "--left-shift");
-        modifierKeys.put(60, "--right-shift");
-        modifierKeys.put(57, "--left-alt");
-        modifierKeys.put(58, "--right-alt");
-        modifierKeys.put(117, "--left-meta");
-        modifierKeys.put(118, "--right-meta");
+		modifierKeys.put(113, "--left-ctrl");
+		modifierKeys.put(114, "--right-ctrl");
+		modifierKeys.put(59, "--left-shift");
+		modifierKeys.put(60, "--right-shift");
+		modifierKeys.put(57, "--left-alt");
+		modifierKeys.put(58, "--right-alt");
+		modifierKeys.put(117, "--left-meta");
+		modifierKeys.put(118, "--right-meta");
 
-        // Translate keycodes into key
-        keyEventCodes.put(29, "a");
-        keyEventCodes.put(30, "b");
-        keyEventCodes.put(31, "c");
-        keyEventCodes.put(32, "d");
-        keyEventCodes.put(33, "e");
-        keyEventCodes.put(34, "f");
-        keyEventCodes.put(35, "g");
-        keyEventCodes.put(36, "h");
-        keyEventCodes.put(37, "i");
-        keyEventCodes.put(38, "j");
-        keyEventCodes.put(39, "k");
-        keyEventCodes.put(40, "l");
-        keyEventCodes.put(41, "m");
-        keyEventCodes.put(42, "n");
-        keyEventCodes.put(43, "o");
-        keyEventCodes.put(44, "p");
-        keyEventCodes.put(45, "q");
-        keyEventCodes.put(46, "r");
-        keyEventCodes.put(47, "s");
-        keyEventCodes.put(48, "t");
-        keyEventCodes.put(49, "u");
-        keyEventCodes.put(50, "v");
-        keyEventCodes.put(51, "w");
-        keyEventCodes.put(52, "x");
-        keyEventCodes.put(53, "y");
-        keyEventCodes.put(54, "z");
-        keyEventCodes.put(131, "f1");
-        keyEventCodes.put(132, "f2");
-        keyEventCodes.put(133, "f3");
-        keyEventCodes.put(134, "f4");
-        keyEventCodes.put(135, "f5");
-        keyEventCodes.put(136, "f6");
-        keyEventCodes.put(137, "f7");
-        keyEventCodes.put(138, "f8");
-        keyEventCodes.put(139, "f9");
-        keyEventCodes.put(140, "f10");
-        keyEventCodes.put(141, "f11");
-        keyEventCodes.put(142, "f12");
+		// Translate keycodes into key
+		keyEventCodes.put(29, "a");
+		keyEventCodes.put(30, "b");
+		keyEventCodes.put(31, "c");
+		keyEventCodes.put(32, "d");
+		keyEventCodes.put(33, "e");
+		keyEventCodes.put(34, "f");
+		keyEventCodes.put(35, "g");
+		keyEventCodes.put(36, "h");
+		keyEventCodes.put(37, "i");
+		keyEventCodes.put(38, "j");
+		keyEventCodes.put(39, "k");
+		keyEventCodes.put(40, "l");
+		keyEventCodes.put(41, "m");
+		keyEventCodes.put(42, "n");
+		keyEventCodes.put(43, "o");
+		keyEventCodes.put(44, "p");
+		keyEventCodes.put(45, "q");
+		keyEventCodes.put(46, "r");
+		keyEventCodes.put(47, "s");
+		keyEventCodes.put(48, "t");
+		keyEventCodes.put(49, "u");
+		keyEventCodes.put(50, "v");
+		keyEventCodes.put(51, "w");
+		keyEventCodes.put(52, "x");
+		keyEventCodes.put(53, "y");
+		keyEventCodes.put(54, "z");
+		keyEventCodes.put(131, "f1");
+		keyEventCodes.put(132, "f2");
+		keyEventCodes.put(133, "f3");
+		keyEventCodes.put(134, "f4");
+		keyEventCodes.put(135, "f5");
+		keyEventCodes.put(136, "f6");
+		keyEventCodes.put(137, "f7");
+		keyEventCodes.put(138, "f8");
+		keyEventCodes.put(139, "f9");
+		keyEventCodes.put(140, "f10");
+		keyEventCodes.put(141, "f11");
+		keyEventCodes.put(142, "f12");
 		keyEventCodes.put(19, "up");
 		keyEventCodes.put(20, "down");
 		keyEventCodes.put(21, "left");
 		keyEventCodes.put(22, "right");
-        keyEventCodes.put(61, "tab");
-        keyEventCodes.put(67, "backspace");
-        keyEventCodes.put(111, "escape");
-        keyEventCodes.put(120, "print"); // and SysRq
-        keyEventCodes.put(116, "scroll-lock");
-        keyEventCodes.put(143, "num-lock");
-        keyEventCodes.put(121, "pause");
-        keyEventCodes.put(124, "insert");
-        keyEventCodes.put(112, "delete");
+		keyEventCodes.put(61, "tab");
+		keyEventCodes.put(67, "backspace");
+		keyEventCodes.put(111, "escape");
+		keyEventCodes.put(120, "print"); // and SysRq
+		keyEventCodes.put(116, "scroll-lock");
+		keyEventCodes.put(143, "num-lock");
+		keyEventCodes.put(121, "pause");
+		keyEventCodes.put(124, "insert");
+		keyEventCodes.put(112, "delete");
 
-        // Chars that are represented by another key + shift
+		// Chars that are represented by another key + shift
 		shiftChars.put("<", ",");
 		shiftChars.put(">", ".");
 		shiftChars.put("?", "/");
@@ -165,37 +165,40 @@ public class MainActivity extends AppCompatActivity {
 		hidGadgetPath = appFileDirectory + "/hid-gadget";
 
 		// Copy over binary (could compare existence/hashes before copying)
-        copyAssets("hid-gadget");
+		copyAssets("hid-gadget");
 
 		dropdownLogging.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-				if(loggingThread != null) {
+				if (loggingThread != null) {
 					// Pretty sure if thread updates tvOutput one more time before finishing being interrupted
 					// it might overwrite the new thread's output until it re-rewrites the output.
 					// Implement locks if this is an issue.
 					loggingThread.interrupt();
 				}
 				String choice = parentView.getSelectedItem().toString();
-				Log.d(TAG,"logging choice: " + choice);
+				Log.d(TAG, "logging choice: " + choice);
 				displayLogs(choice);
 			}
 
 			@Override
-			public void onNothingSelected(AdapterView<?> parentView) {}
+			public void onNothingSelected(AdapterView<?> parentView) {
+			}
 		});
 
 
 		etInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {}
+			@Override
+			public void afterTextChanged(Editable s) {
+			}
 
 			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
 
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				Log.d(TAG,"Diff: " + s);
+				Log.d(TAG, "Diff: " + s);
 				Log.d(TAG, start + " " + before + " " + count);
 				if (s.length() >= 1) {
 					char newChar = s.subSequence(s.length() - 1, s.length()).charAt(0);
@@ -205,40 +208,40 @@ public class MainActivity extends AppCompatActivity {
 				}
 			}
 		});
-    }
+	}
 
-    // detects non-printing keys
-    // TODO: handle issues of edittext watcher and onKeyDown listener detecting the same press
-    //       currently not an issue, but it might become one later
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (KeyEvent.isModifierKey(keyCode)) {
-            modifier = modifierKeys.get(event.getKeyCode());
-            nextKeyModified = true;
+	// detects non-printing keys
+	// TODO: handle issues of edittext watcher and onKeyDown listener detecting the same press
+	//       currently not an issue, but it might become one later
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (KeyEvent.isModifierKey(keyCode)) {
+			modifier = modifierKeys.get(event.getKeyCode());
+			nextKeyModified = true;
 			Log.d(TAG, "modifier: " + modifier);
-        }
+		}
 
-        String str = null;
-        if ((str = keyEventCodes.get(event.getKeyCode())) != null) {
-            sendKey(str);
+		String str = null;
+		if ((str = keyEventCodes.get(event.getKeyCode())) != null) {
+			sendKey(str);
 			Log.d(TAG, "onKeyDown key: " + str);
-        }
+		}
 		Log.d(TAG, "keycode: " + event.getKeyCode());
-        return true;
-    }
+		return true;
+	}
 
 	private void sendKey(String key) {
-		if(key == null) {
+		if (key == null) {
 			Log.e(TAG, "sendKey received null key value");
 			return;
 		}
 
 		String options = "";
 		String adjustedKey = key;
-		if(nextKeyModified) {
-		    options += modifier;
-		    nextKeyModified = false;
-        }
+		if (nextKeyModified) {
+			options += modifier;
+			nextKeyModified = false;
+		}
 		String str = null;
 		if ((str = shiftChars.get(key)) != null) {
 			adjustedKey = str;
@@ -341,7 +344,7 @@ public class MainActivity extends AppCompatActivity {
 		runOnUiThread(() -> tvOutput.setText("No Output"));
 
 		// Trim filter down to just the first letter because that's what logcat uses to filter
-		String verbosityLetter = verbosityFilter.substring(0,1);
+		String verbosityLetter = verbosityFilter.substring(0, 1);
 		loggingThread = new Thread(() -> {
 			try {
 				String command = String.format("logcat -s hid-client:%s -v raw", verbosityLetter);
@@ -361,12 +364,11 @@ public class MainActivity extends AppCompatActivity {
 						runOnUiThread(() -> tvOutput.setText(log.toString()));
 					}
 				}
-			}
-			catch (IOException e) {
+			} catch (IOException e) {
 				Log.e("tag", "ioexc in logging");
 			}
 		});
 		loggingThread.start();
-		Log.d("tag","logging started with verbosity: " + verbosityFilter);
+		Log.d("tag", "logging started with verbosity: " + verbosityFilter);
 	}
 }
