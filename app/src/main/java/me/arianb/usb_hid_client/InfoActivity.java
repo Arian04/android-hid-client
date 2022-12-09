34fb68b934d5f64ed80d462410e7affe68c42e03
Arian Baishya <arian.baishya2004@gmail.com>
2022-12-08 20:48:50 -0600

    Finish info page

--- app/src/main/java/me/arianb/usb_hid_client/InfoActivity.java
+++ app/src/main/java/me/arianb/usb_hid_client/InfoActivity.java
@@ -1,7 +1,12 @@
 package me.arianb.usb_hid_client;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
@@ -10,5 +15,23 @@ public class InfoActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info);
+
+		setTitle(R.string.info);
+
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
+
+		// Make link clickable
+		TextView tvInfoLink = findViewById(R.id.tvInfoLink);
+		tvInfoLink.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Make activity back button work
+		Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+		startActivity(intent);
+		return true;
 	}
 }
