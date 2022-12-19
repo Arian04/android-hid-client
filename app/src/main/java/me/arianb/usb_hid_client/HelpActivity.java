5c0e333516c73b94dc0e3a13ce4b9a692e01db87
Arian Baishya <arian.baishya2004@gmail.com>
2022-12-19 15:43:49 -0600

    Finish help page

--- app/src/main/java/me/arianb/usb_hid_client/HelpActivity.java
+++ app/src/main/java/me/arianb/usb_hid_client/HelpActivity.java
@@ -1,7 +1,12 @@
 package me.arianb.usb_hid_client;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HelpActivity extends AppCompatActivity {
@@ -10,5 +15,27 @@ public class HelpActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
+
+		setTitle(R.string.help);
+
+		// Enable back button
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
+
+		// Make links clickable
+		TextView tvHelpFAQ;
+		tvHelpFAQ = findViewById(R.id.tvHelpFAQ_a1);
+		tvHelpFAQ.setMovementMethod(LinkMovementMethod.getInstance());
+		tvHelpFAQ = findViewById(R.id.tvHelpFAQ_a3);
+		tvHelpFAQ.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Make activity back button work
+		Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+		startActivity(intent);
+		return true;
 	}
 }
