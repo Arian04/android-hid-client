9644e967016bf5bd745fe4f34a64bf2c647d8297
Arian Baishya <arian.baishya2004@gmail.com>
2024-03-25 22:34:03 -0500

    cleanup: remove unused import

--- app/src/main/java/me/arianb/usb_hid_client/MainActivity.java
+++ app/src/main/java/me/arianb/usb_hid_client/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
