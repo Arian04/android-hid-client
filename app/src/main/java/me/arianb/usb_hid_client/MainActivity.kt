e614886448511cde261385e3136a428cf43d8848
Arian Baishya <arian.baishya2004@gmail.com>
2024-06-26 17:24:59 -0500

    split MainActivity code into functions

--- app/src/main/java/me/arianb/usb_hid_client/MainActivity.kt
+++ app/src/main/java/me/arianb/usb_hid_client/MainActivity.kt
@@ -3,10 +3,12 @@ package me.arianb.usb_hid_client
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import cafe.adriel.voyager.core.annotation.ExperimentalVoyagerApi
+import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.NavigatorDisposeBehavior
 import cafe.adriel.voyager.transitions.SlideTransition
@@ -22,51 +24,59 @@ import timber.log.Timber.DebugTree
 // 		will be referred to as a "shifted" key. In the previous example, 2, 4, and ] would be considered
 // 		the "un-shifted" keys.
 class MainActivity : ComponentActivity() {
-    private val settingsViewModel: SettingsViewModel by viewModels()
-
-    @OptIn(ExperimentalVoyagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // This is separated from the debug log code below in the off chance that I need to see something that's been
-        // logged before preferences are read.
+        // This is separated from the production logging code below in the off chance that I need to see something
+        // that's been logged before preferences are read.
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
             Shell.enableVerboseLogging = true
         }
 
         setContent {
-            val userPreferencesState by settingsViewModel.userPreferencesFlow.collectAsState()
-
-            if (!BuildConfig.DEBUG) {
-                val isDebugModeEnabled = userPreferencesState.isDebugModeEnabled
-                if (isDebugModeEnabled) {
-                    Timber.plant(DebugTree())
-                    Shell.enableVerboseLogging = true
-                } else {
-                    Timber.uprootAll()
-                    Shell.enableVerboseLogging = false
-                }
-            }
-
-            // If this is the first time the app has been opened, then show OnboardingActivity
-            val onboardingDone = userPreferencesState.isOnboardingDone
-            val startScreen = if (!onboardingDone) {
-                OnboardingScreen()
-            } else {
-                MainScreen()
-            }
-
-            // dispose params are due to this issue: https://github.com/adrielcafe/voyager/issues/106
-            Navigator(
-                startScreen,
-                disposeBehavior = NavigatorDisposeBehavior(disposeSteps = false),
-            ) { navigator ->
-                SlideTransition(
-                    navigator,
-                    disposeScreenAfterTransitionEnd = true
-                )
-            }
+            Entrypoint()
         }
+    }
+}
+
+@Composable
+fun Entrypoint(settingsViewModel: SettingsViewModel = viewModel()) {
+    val userPreferencesState by settingsViewModel.userPreferencesFlow.collectAsState()
+
+    if (!BuildConfig.DEBUG) {
+        val isDebugModeEnabled = userPreferencesState.isDebugModeEnabled
+        if (isDebugModeEnabled) {
+            Timber.plant(DebugTree())
+            Shell.enableVerboseLogging = true
+        } else {
+            Timber.uprootAll()
+            Shell.enableVerboseLogging = false
+        }
+    }
+
+    // If this is the first time the app has been opened, then show OnboardingActivity
+    val onboardingDone = userPreferencesState.isOnboardingDone
+    val startScreen = if (!onboardingDone) {
+        OnboardingScreen()
+    } else {
+        MainScreen()
+    }
+
+    ScreenEntrypoint(startScreen)
+}
+
+@OptIn(ExperimentalVoyagerApi::class)
+@Composable
+fun ScreenEntrypoint(startScreen: Screen) {
+    // dispose params are due to this issue: https://github.com/adrielcafe/voyager/issues/106
+    Navigator(
+        startScreen,
+        disposeBehavior = NavigatorDisposeBehavior(disposeSteps = false),
+    ) { navigator ->
+        SlideTransition(
+            navigator,
+            disposeScreenAfterTransitionEnd = true
+        )
     }
 }
