b1937c5d7a7520c17e2e365eac85bb8f967d8798
Arian Baishya <arian.baishya2004@gmail.com>
2026-02-02 21:57:02 -0600

    reduce size of (currently unused) PaddingExtraSmall constant

--- app/src/main/java/me/arianb/usb_hid_client/ui/theme/Dimensions.kt
+++ app/src/main/java/me/arianb/usb_hid_client/ui/theme/Dimensions.kt
@@ -3,7 +3,7 @@ package me.arianb.usb_hid_client.ui.theme
 import androidx.compose.ui.unit.dp
 
 val PaddingNone = 0.dp
-val PaddingExtraSmall = 4.dp
+val PaddingExtraSmall = 2.dp
 val PaddingSmall = 8.dp
 val PaddingNormal = 16.dp
 val PaddingLarge = 24.dp
